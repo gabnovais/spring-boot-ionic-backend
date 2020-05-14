aaccd4f8f0be0154d398634b3273d743c09d30f2
Gabriel Novais <gsnbil@gmail.com>
2020-05-14 20:28:36 -0300

    Estado e Cidade

---
+++ src/main/java/com/umlgabriel/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.umlgabriel.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.umlgabriel.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}
