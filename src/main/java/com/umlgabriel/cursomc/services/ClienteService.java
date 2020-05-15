cbfa4bca65a5ffba0f44612376516b7e8cd8887e
Gabriel Novais <gsnbil@gmail.com>
2020-05-15 20:48:24 -0300

    Endpoint/Cliente/{id} disponivel

---
+++ src/main/java/com/umlgabriel/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.umlgabriel.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.umlgabriel.cursomc.domain.Cliente;
+import com.umlgabriel.cursomc.repositories.ClienteRepository;
+import com.umlgabriel.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
